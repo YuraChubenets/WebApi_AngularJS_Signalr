023754f45da300f8c7321ed2ef42f2eb9281610d
YuraChubenets <u.chubenets@gmail.com>
2016-04-18 22:08:23 +0300

    feature angular material design

--- WebClient/App/productController.js
+++ WebClient/App/productController.js
@@ -1,17 +1,23 @@
-﻿angular.module("restApp", [])
+﻿
+angular.module("restApp", ["ngMaterial"])
               .controller("productController", ["$scope", "$http", function ($scope, $http) {
-                  //$http.get("http://localhost:11962/api/product").success(function (response) {
-                  //    $scope.result = response;
-                  $scope.result = [
-                                        { id: '1', Name: 'Product 1', Price: 110, Quantity: 2 },
-                                        { id: '2', Name: 'Product 2', Price: 110, Quantity: 8 },
-                                        { id: '3', Name: 'Product 3', Price: 120, Quantity: 5 },
-                                        { id: '4', Name: 'Product 4', Price: 10, Quantity: 3 },
-                                        { id: '5', Name: 'Product 5', Price: 170, Quantity: 2 },
-                                        { id: '6', Name: 'Product 6', Price: 160, Quantity: 1 },
-                                        { id: '7', Name: 'Product 7', Price: 45, Quantity: 7 }
-                  ];
 
+                  $scope.isProgress = true;
+
+                  $http.get("http://localhost:60780/api/products").success(function (response) {
+                      // $scope.result = response;
+                      //mock
+                      $scope.result = [
+                                            { id: '1', Name: 'Product 1', Price: 110, Quantity: 2 },
+                                            { id: '2', Name: 'Product 2', Price: 110, Quantity: 8 },
+                                            { id: '3', Name: 'Product 3', Price: 120, Quantity: 5 },
+                                            { id: '4', Name: 'Product 4', Price: 10, Quantity: 3 },
+                                            { id: '5', Name: 'Product 5', Price: 170, Quantity: 2 },
+                                            { id: '6', Name: 'Product 6', Price: 160, Quantity: 1 },
+                                            { id: '7', Name: 'Product 7', Price: 45, Quantity: 7 }
+                      ];
+                      $scope.isProgress = false;
+                  });
                   ///------------DELETE---------------
                   $scope.del = function (id) {
                       var answer = confirm("Are you sure?");
--- WebClient/Scripts/_references.js
+++ WebClient/Scripts/_references.js
@@ -1,2 +1,6 @@
 ﻿/// <reference path="angular.js" />
 /// <reference path="jquery-1.9.1.intellisense.js" />
+/// <reference path="angular-material/angular-material.js" />
+/// <reference path="angular-aria/angular-aria.min.js" />
+/// <reference path="angular-animate/angular-animate.min.js" />
+
